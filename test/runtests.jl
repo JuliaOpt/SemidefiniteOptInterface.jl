b71e3b4ceac857d9a6ba7e158cc6a774e9dcedde
Benoît Legat <benoit.legat@gmail.com>
2017-07-22 14:43:13 +0200

    Use conic tests in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,11 @@ using Base.Test
 using CSDP
 solvers = [CSDP.CSDPSolver()]
 
-@testset "Linear tests with $solver" for solver in solvers
-    include(joinpath(Pkg.dir("MathOptInterface"), "test", "contlinear.jl"))
-    contlineartest(solver, 1e-7)
-end
-#@testset "Conic tests with $solver" for solver in solvers
-#    include(joinpath(Pkg.dir("MathOptInterface"), "test", "contconic.jl"))
-#    contconictest(solver, 1e-7)
+#@testset "Linear tests with $solver" for solver in solvers
+#    include(joinpath(Pkg.dir("MathOptInterface"), "test", "contlinear.jl"))
+#    contlineartest(solver, 1e-7)
 #end
+@testset "Conic tests with $solver" for solver in solvers
+    include(joinpath(Pkg.dir("MathOptInterface"), "test", "contconic.jl"))
+    contconictest(solver, 1e-7)
+end
