07cd796794ea4b215065a8187482e3d5d087d383
Benoît Legat <benoit.legat@gmail.com>
2017-11-24 23:05:20 +0100

    Update with MOIT new option

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,12 +7,14 @@ solvers = [() -> CSDP.CSDPInstance(printlevel=0)]
 using MathOptInterfaceTests
 const MOIT = MathOptInterfaceTests
 
+const config = MOIT.TestConfig(1e-7, 1e-7, true, true, true, true)
 # Need 1e-5 for geomean test
-const config = MOIT.TestConfig(1e-5, 1e-5, true, true, true)
+const configgeomean = MOIT.TestConfig(1e-5, 1e-5, true, true, true, true)
 
 @testset "Linear tests with $solver" for solver in solvers
     MOIT.contlineartest(solver, config)
 end
 @testset "Conic tests with $solver" for solver in solvers
-    MOIT.contconictest(solver, config, ["logdet", "rootdet1qv", "rootdet1qf"])
+    MOIT.contconictest(solver, config, ["geomean", "logdet", "rootdet1qv", "rootdet1qf"])
+    MOIT.geomeantest(solver, configgeomean)
 end
